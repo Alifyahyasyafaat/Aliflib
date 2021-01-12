903bd5b6a976206427d20d314c0532ec4834188e
Alifyahyasyafaat <yahyasyafaat15@gmail.com>
2021-01-12 20:01:12 +0700

    AlifLibrary

---
+++ Aliflibrary/src/main/java/com/example/aliflibrary/Kirim/TentangSaya.java
@@ -0,0 +1,16 @@
+package com.example.aliflibrary.Kirim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aliflibrary.R;
+
+public class TentangSaya extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_saya);
+    }
+}
